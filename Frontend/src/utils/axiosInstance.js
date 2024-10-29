8a3881df2fdc2b9c088a6216e80d24e22c5f6c4e
harshal mali <20harshalmali@gmail.com>
2024-10-30 00:05:32 +0530

    deployment

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:5000/api", // Change to your API's base URL
+	baseURL: "https://menu-world.onrender.com", // Change to your API's base URL
 	timeout: 10000, // Set a timeout for requests
 });
 
