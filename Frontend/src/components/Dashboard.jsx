a8a4792dacb65c4971b6849899217aa1f4d879c6
harshal mali <20harshalmali@gmail.com>
2024-11-28 02:08:21 +0530

    .

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
 	return (
 		<div className="min-h-screen mt-16   bg-gray-300 text-white p-8 flex flex-col dark:bg-gray-800">
 			<h2 className="text-3xl text-gray-800 dark:text-gray-200 font-bold mb-6">Dashboard</h2>
-			<div className="mb-4 flex align-middle justify-between sm:justify-normal">
+			<div className="mb-4 text-xs sm:text-sm flex align-middle  justify-between sm:justify-normal">
 				<Link
 					to="/create-menu"
 					className="mr-8  bg-blue-600 hover:bg-blue-500 dark:bg-blue-500 dark:hover:bg-blue-400 text-white font-semibold p-2 rounded transition"
